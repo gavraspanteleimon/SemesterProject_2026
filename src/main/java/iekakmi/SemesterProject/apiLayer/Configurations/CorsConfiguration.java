f38b32b3a11662d06e22af908d43aa2802a36bb9
Pantelis <gavraspantelis@gmail.com>
2026-05-10 21:03:00 +0300

    axios training

--- src/main/java/iekakmi/SemesterProject/apiLayer/Configurations/CorsConfiguration.java
+++ src/main/java/iekakmi/SemesterProject/apiLayer/Configurations/CorsConfiguration.java
@@ -4,12 +4,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
-public class CorsConfiguration implements WebMvcConfigurer{
-	
-	@Override
-	public void addCorsMappings(CorsRegistry reg)
-	{
-		reg.addMapping("/**").allowedMethods("*");
-	}
-	
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry reg) {
+        reg.addMapping("/**")
+           .allowedOrigins("*")   // ← αυτό έλειπε
+           .allowedMethods("*");
+    }
 }
